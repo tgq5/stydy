aeae696d4cb360be30a9a98528cbb8f45c0a02dc
Thiago G. Q. <thiagogarianiq@gmail.com>
2025-02-04 08:47:57 -0300

    Run PyDriller for elasticsearch

--- format.py
+++ format.py
@@ -2,10 +2,6 @@ import json
 import os
 from utils.utils import indent_file, update_matched
 
-# for file in os.listdir('./json/bics/'):
-#     file_path = f'./json/bics/{file}'
-#     indent_file(file_path)
-
-# indent_file("./json/elasticsearch/issues.json")
-indent_file("./json/ansible/apache.json")
-indent_file("./json/ansible/apache_2.json")
+for file in os.listdir('./json/bics/'):
+    file_path = f'./json/bics/{file}'
+    indent_file(file_path)
